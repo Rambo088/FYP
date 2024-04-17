91938a4e3a94ebf3f77092bc1ae277d9a518d3eb
Rayan Abdulsmad <116089899+Rambo088@users.noreply.github.com>
2024-04-17 04:01:17 +0100

    Add files via upload

---
+++ FYP_FinalYear/preprocess.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from tensorflow.keras.optimizers import SGD
+
+
+class DataProcessor:
+    def __init__(self, data):
+        self.data = data
+
+    def preprocess_data(data):
+        # Assuming 'DateTime' column is in the format of 'yyyy-mm-dd hh:mm:ss'
+        data['DateTime'] = pd.to_datetime(data['DateTime'])
+        # Extracting features from datetime
+        data['hour'] = data['DateTime'].dt.hour
+        data['day'] = data['DateTime'].dt.day
+        data['weekday'] = data['DateTime'].dt.weekday
+        # Dropping ID column
+        data.drop(columns=['ID'], inplace=True)
+
+        return data
+
+
+data_path = r"C:\Users\rayan\OneDrive\Documents\Year-3\Semester 1\Individual Project\Datasets\traffic.csv"
+data = pd.read_csv(data_path)
